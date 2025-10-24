04bcddd52961b7c387a561aa3d2770ef05e9326c
Maria Zhenru Lopez Gomez <mzhenrulg69@educastur.es>
2025-10-24 20:49:11 +0200

    lombok

--- src/main/java/com/example/biblioteca/Entity/Libro.java
+++ src/main/java/com/example/biblioteca/Entity/Libro.java
@@ -1,4 +1,21 @@
 package com.example.biblioteca.Entity;
 
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="Libro")
 public class Libro {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column
+    private String autor;
 }
