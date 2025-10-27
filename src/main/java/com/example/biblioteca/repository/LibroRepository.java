2e2635756333aea3f1c7950ee21ebf0da8522a43
Maria Zhenru Lopez Gomez <mzhenrulg69@educastur.es>
2025-10-27 20:07:36 +0100

    cambio version y creacion libro repository

---
+++ src/main/java/com/example/biblioteca/repository/LibroRepository.java
@@ -0,0 +1,10 @@
+package com.example.biblioteca.repository;
+
+import com.example.biblioteca.Entity.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+
+}
